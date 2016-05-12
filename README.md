657914277f228b892eca37cea4dd88e7e5eb37eb
aaron <aarongitis@gmail.com>
2016-05-12 12:39:41 -0700

    Add initial project files and README

---
+++ README.md
@@ -0,0 +1,2 @@
+##BoxPlayer
+####A simple music player that will play your mp3's!
---
+++ app/src/main/java/badassapps/aaron/boxplayer/MainActivity.java
@@ -0,0 +1,14 @@
+package badassapps.aaron.boxplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Use Material Design Guidelines
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
